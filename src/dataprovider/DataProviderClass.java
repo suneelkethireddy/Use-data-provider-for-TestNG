7593ab4fa5199f86762561de96f4c8ad2ea440b7
suneelkethireddy <suneelkumar.kethireddy@gmail.com>
2016-12-06 23:49:02 -0800

    Addition

--- src/dataprovider/DataProviderClass.java
+++ src/dataprovider/DataProviderClass.java
@@ -8,7 +8,11 @@ public class DataProviderClass {
 	
 	@DataProvider(name="getLoginData")
 	public static  Object[][] getLoginData() throws IOException{
+		System.out.println("Good to see to here");
 		return ReadFromExcelFile.getExcelData("Login.xlsx", "LoginDetails");
+		
+		
+		
 	}
 	
 }
